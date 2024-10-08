1dc5631b4c6528ce43b336b00166f58156eaecf9
DenisEsipko <iesypko@gmail.com>
2024-10-08 21:54:52 +0300

    added solutions

--- core-js-numbers-main/src/numbers-tasks.js
+++ core-js-numbers-main/src/numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
@@ -33,8 +33,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * radius * Math.PI;
 }
 
 /**
@@ -42,15 +42,16 @@ function getCircleCircumference(/* radius */) {
  *
  * @param {number} value1
  * @param {number} value2
- * @return {number}
+ * @return {bigint}
  *
  * @example:
  *   5, 5  => 5
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  // eslint-disable-next-line no-undef
+  return (BigInt(value1) + BigInt(value2)) / BigInt(2);
 }
 
 /**
@@ -68,8 +69,8 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
@@ -84,8 +85,11 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  if (a === 0) {
+    throw new Error("Coefficient 'a' cannot be zero for a linear equation.");
+  }
+  return -b / a;
 }
 
 /**
@@ -105,8 +109,32 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const magnitudeA = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const magnitudeB = Math.sqrt(x2 ** 2 + y2 ** 2);
+
+  if (magnitudeA === 0 || magnitudeB === 0) {
+    throw new Error('One of the vectors has zero length');
+  }
+
+  const cosTheta = dotProduct / (magnitudeA * magnitudeB);
+
+  // Возвращает расстояние между двумя точками в декартовых координатах.
+  const clampedCosTheta = Math.max(-1, Math.min(1, cosTheta));
+
+  return Math.acos(clampedCosTheta);
+  // Найти угол между векторами a = {3; 4} и b = {4; 3}.
+  //
+  // Решение: Найдем скалярное произведение векторов:
+  // a·b = 3 · 4 + 4 · 3 = 12 + 12 = 24.
+  //
+  // Найдем модули векторов:
+  // |a| = √(3**2 + 4**2) = √9 + √16 = √25 = 5
+  // |b| = √(4**2 + 3**2) = √16 + 9 = √25 = 5
+  //
+  // Найдем угол между векторами:
+  // cos α =  	скалярное произведение 	 /  (модуль вектора * модкль вектора)
 }
 
 /**
@@ -122,8 +150,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -137,8 +165,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -154,8 +182,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a * a + b * b + c * c);
 }
 
 /**
@@ -175,8 +203,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const factor = Math.pow(10, pow);
+  return Math.round(num / factor) * factor;
 }
 
 /**
@@ -196,8 +225,16 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+
+  return true;
 }
 
 /**
@@ -215,8 +252,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+  return isNaN(num) ? def : num;
 }
 
 /**
@@ -230,8 +268,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num * num * num;
 }
 
 /**
@@ -247,8 +285,22 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index < 0) return undefined; // Обработка случая отрицательного индекса
+  if (index === 0) return 0; // вариант для индекса 0
+  if (index === 1) return 1; // вариант для индекса 1
+
+  let a = 0; // F(0)
+  let b = 1; // F(1)
+  let fib;
+
+  for (let i = 2; i <= index; i++) {
+    fib = a + b; // F(n) = F(n-1) + F(n-2)
+    a = b; //  следующему числу Фибоначчи
+    b = fib; // Обновите b до последнего числа Фибоначчи.
+  }
+
+  return fib; // Вернуть число Фибоначчи по указанному индексу
 }
 
 /**
@@ -262,8 +314,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+
+//Используйте формулу (n×(n+1))/2
+// который эффективно вычисляет сумму первых nn натуральных чисел.
+function getSumToN(n) {
+  if (n < 1) return 0;
+  return (n * (n + 1)) / 2;
 }
 
 /**
@@ -277,8 +333,11 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+
+  return String(Math.abs(num)) // Обрабатывайте отрицательные числа, принимая абсолютное
+    .split('')
+    .reduce((sum, digit) => sum + Number(digit), 0);
 }
 
 /**
@@ -292,8 +351,11 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+//Побитовая операция : выражение (num & (num - 1)) === 0 эффективно проверяет, является ли число степенью двойки:
+//Это работает, потому что в двоичном представлении степени двойки установлен ровно один бит. Например, 44 это 100100 в двоичном формате и 33 это 011011 . Когда вы вычитаете 11 из 44 , ты получишь 33, который переворачивает все биты после крайнего правого 11 , таким образом, 100 и 011 = 000 .
+function isPowerOfTwo(num) {
+  if (num <= 0) return false; // положительные числа.
+  return (num & (num - 1)) === 0; // Проверка является ли число степенью двойки
 }
 
 /**
@@ -306,8 +368,8 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
 }
 
 /**
@@ -321,8 +383,12 @@ function getSine(/* num */) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  if (base < 2 || base > 36) {
+    throw new Error('Base must be between 2 and 36');
+  }
+
+  return number.toString(base);
 }
 
 /**
@@ -335,8 +401,8 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
 }
 
 /**
@@ -350,8 +416,8 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits); // Use the built-in toFixed method
 }
 
 /**
